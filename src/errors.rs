6379ffaa5c0cf57246506d7704e5126c043defa1
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-17 15:12:18 -0500

    add new draft of continous file streaming function

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,5 @@
-pub enum FTPErrors {
+pub enum FTPError {
     CannotOpenFile,
-    CannotRead
+    CannotRead,
+    CannotWrite
 }
--- src/ftp_write.rs
+++ src/ftp_write.rs
@@ -1,3 +1,38 @@
 use std::io::{Write, Read};
 use std::fs::File;
-use crate::errors::FTPErrors;
+use crate::errors::FTPError;
+
+pub fn stream_file<W: Write>(stream: &mut W, filename: &str) -> Result<(), FTPError> {
+    match File::open(filename) {
+        Ok(mut file) => {
+            let mut buff: [u8; 256] = [0; 256];
+
+            loop {
+                match file.read(&mut buff) {
+                    Ok(size) => {
+                        if size == 0 {
+                            return Ok(());
+                        } else {
+                            match stream.write_all(&buff[0..size]) {
+                                Ok(_) => {
+
+                                },
+                                Err(_) => {
+                                    return Err(FTPError::CannotWrite);
+                                }
+                            }
+                        }
+                    },
+                    Err(_) => {
+                        return Err(FTPError::CannotRead);
+                    }
+                }
+            };
+
+            Ok(())
+        },
+        Err(_) => {
+            Err(FTPError::CannotOpenFile)
+        }
+    }
+}
