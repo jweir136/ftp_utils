dda5c82dff60dbef3a89564305c230233bbc3127
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-18 12:20:26 -0500

    add docs for stream_to_file function

--- src/ftp_read.rs
+++ src/ftp_read.rs
@@ -2,6 +2,24 @@ use crate::errors::FTPError;
 use std::io::{Write, Read};
 use std::fs::File;
 
+/// Description:
+///     This function is used to continously write the bytes from a streamable item to a file.
+///
+/// Arguments:
+///     stream: &mut Write      =>  This arugment is used to specify the item to write the bytes to.
+///                                     All items passed must implement the 'Write' trait.
+///     filename: &str          =>  This argument is the complete path and filename to the file
+///                                     to create and read the bytes from.
+///
+/// Points of Failure:
+///     CannotWrite       =>      This error occurs whenever the streamable item cannot have bytes written to it.
+///     CannotRead        =>      This occurs whenever the instant of the file cannot be read from.
+///     CannotCreateFile  =>      This occurs whenever the given file cannot be created.
+///
+/// Dev. Notes:
+///     (1)     The ```return``` statements are intentional. They were used over the most 'rust prefered'
+///                 method. The ```return``` statements are used to that the multiple ```match``` statements may be used 
+///                 may be used in order to account for custom error handling of all errors.
 pub fn stream_to_file<W: Write + Read>(stream: &mut W, filepath: &str) -> Result<(), FTPError> {
     match File::create(filepath) {
         Ok(mut file) => {
